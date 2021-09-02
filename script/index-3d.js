0626cf68033cf0ca1206cfc551098159a993c8b5
gxllium <gxllium@seznam.cz>
2021-09-02 13:16:25 +0000

    Adding Three.js Scene to Header

--- script/index-3d.js
+++ script/index-3d.js
@@ -1,1 +1,17 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
+
+const pageHeader = document.querySelector('.page-header');
+const scene = new THREE.Scene();
+
+let cameraFov    = 75;
+let cameraAspect = window.innerWidth / window.innerHeight;
+let cameraNear   = 0.1;
+let cameraFar    = 1000;
+
+const camera = new THREE.PerspectiveCamera(cameraFov, cameraAspect, cameraNear, cameraFar);
+
+const renderer = new THREE.WebGLRenderer(scene, camera);
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+renderer.domElement.classList.add('header-background');
+pageHeader.appendChild(renderer.domElement);
